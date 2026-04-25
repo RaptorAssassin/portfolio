b7ec1f0462df4d4b422d04f87926861eab5f9710
Raptor <karl.b.albrecht@gmail.com>
2026-04-25 17:02:23 +0200

    initial commit

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link';
+
 export const NavBar = () => {
   return (
     <div className="bg-accent/20 absolute top-5 left-1/2 -translate-x-1/2 rounded-full px-4 py-2 backdrop-blur-md">
-      <nav className=""></nav>
+      <nav className="">
+        <Link href="#">Home</Link>
+      </nav>
     </div>
   );
 };
